0db77962daeb55f9d794bf68666c77e2d1d5bccd
UnsignedInt8 <unsignedint8@gmail.com>
2021-10-28 06:28:46 +0800

    [UI] Design backup words

--- screens/land/backup/index.tsx
+++ screens/land/backup/index.tsx
@@ -1,15 +1,17 @@
-import { SafeAreaView, Text, TouchableHighlight, View } from 'react-native';
+import { SafeAreaView, ScrollView, Text, TouchableHighlight, View } from 'react-native';
+import { borderColor, fontColor, secondaryFontColor } from '../../../constants/styles';
 
 import { Button } from '../../../components';
+import { Ionicons } from '@expo/vector-icons';
 import React from 'react';
-import { borderColor } from '../../../constants/styles';
+import { TouchableOpacity } from 'react-native-gesture-handler';
 import { observer } from 'mobx-react-lite';
 import styles from '../styles';
 
 const phrases = 'brisk casual lunch sudden trust path impose october prosper chunk deposit claw become oil strike'.split(' ');
 
 export default observer(() => {
-  const renderStaticWord = (words: string[]) => (
+  const renderStaticWords = (words: string[]) => (
     <View style={{ flexDirection: 'row', flexWrap: 'wrap' }}>
       {words.map((word, index) => (
         <TouchableHighlight
@@ -23,7 +25,7 @@ export default observer(() => {
             borderWidth: 1,
             borderRadius: 7,
             marginEnd: 12,
-            marginBottom: 12,
+            marginBottom: 8,
           }}
         >
           <Text style={{}}>{word}</Text>
@@ -32,30 +34,62 @@ export default observer(() => {
     </View>
   );
 
+  const renderSortedWords = (words: string[]) => (
+    <ScrollView
+      contentContainerStyle={{
+        padding: 12,
+      }}
+      style={{
+        backgroundColor: borderColor,
+        borderRadius: 10,
+        borderWidth: 1,
+        borderColor,
+        marginVertical: 12,
+        maxHeight: 200,
+      }}
+    >
+      <View style={{ flexDirection: 'row', flexWrap: 'wrap' }}>
+        {words.map((word, index) => (
+          <View
+            key={index}
+            style={{
+              padding: 12,
+              paddingEnd: 0,
+              paddingVertical: 0,
+              borderColor: secondaryFontColor,
+              borderWidth: 1,
+              borderRadius: 7,
+              marginEnd: 12,
+              marginBottom: 8,
+              flexDirection: 'row',
+              alignItems: 'center',
+            }}
+          >
+            <Text style={{ fontSize: 14 }}>{word}</Text>
+            <TouchableOpacity
+              style={{
+                paddingStart: 8,
+                paddingVertical: 8,
+                paddingEnd: 8,
+                marginBottom: -2,
+              }}
+            >
+              <Ionicons name="close" size={12} color={fontColor} />
+            </TouchableOpacity>
+          </View>
+        ))}
+      </View>
+    </ScrollView>
+  );
+
   return (
     <SafeAreaView style={styles.safeArea}>
       <View style={styles.rootContainer}>
         <Text>Please sort the words correctly. </Text>
 
-        <View
-          style={{
-            flexDirection: 'row',
-            flexWrap: 'wrap',
-            backgroundColor: borderColor,
-            borderRadius: 10,
-            borderWidth: 1,
-            borderColor,
-            marginVertical: 12,
-            height: 200,
-            padding: 12,
-          }}
-        >
-          <View>
-            <Text></Text>
-          </View>
-        </View>
+        {renderSortedWords([...phrases].slice(0, 24))}
 
-        {renderStaticWord(phrases)}
+        {renderStaticWords(phrases)}
 
         <View style={{ flex: 1 }} />
         <Button title="Next" />
