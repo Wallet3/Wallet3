9a2978b78a186df5da3ab4e8af1ae4a19e82a822
UnsignedInt8 <unsignedint8@gmail.com>
2021-11-26 16:53:03 +0800

    [VM] Support wallet3 scheme

--- viewmodels/UrlHub.ts
+++ viewmodels/UrlHub.ts
@@ -7,6 +7,12 @@ class UrlHub {
   private lastHandled = 0;
 
   start() {
+    console.log(
+      Linking.parse(
+        'wallet3://wc?uri=wc%3A445aee77-4115-47d1-8d05-3f2abb88ebd7%401%3Fbridge%3Dhttps%253A%252F%252Fw.bridge.walletconnect.org%26key%3Da3f9fd25769a0339692d1eca55268942ec7c11f65c76a8348eee9f2b0db09af0'
+      )
+    );
+
     Linking.getInitialURL().then((url) => this.handleURL(url!));
     Linking.addEventListener('url', ({ url }) => this.handleURL(url));
   }
@@ -23,13 +29,20 @@ class UrlHub {
       return false;
     }
 
-    PubSub.publish(`CodeScan-${scheme}`, { data: url });
+    if (scheme === 'wallet3:') {
+      try {
+        const { queryParams } = Linking.parse(url);
+        PubSub.publish(`CodeScan-wc:`, { data: queryParams.uri });
+      } catch (error) {}
+    } else {
+      PubSub.publish(`CodeScan-${scheme}`, { data: url });
+    }
 
     return true;
   };
 
   findScheme(data: string) {
-    const supportedSchemes = ['ethereum', 'wc:', '0x'];
+    const supportedSchemes = ['ethereum', 'wc:', 'wallet3:', '0x'];
     return (
       supportedSchemes.find((schema) => data.toLowerCase().startsWith(schema)) || (data.endsWith('.eth') ? '0x' : undefined)
     );
