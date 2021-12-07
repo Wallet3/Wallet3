55d96fe1e646b4b8833f30ccddd1bf7b80f8c0b4
UnsignedInt8 <unsignedint8@gmail.com>
2021-12-07 15:10:07 +0800

    [UI] Add social UI layout

--- screens/profile/index.tsx
+++ screens/profile/index.tsx
@@ -1,30 +1,94 @@
-import { StatusBar as RNStatusBar, View } from 'react-native';
 import React, { useEffect } from 'react';
+import { Text, View } from 'react-native';
+import { secondaryFontColor, thirdFontColor } from '../../constants/styles';
 
 import App from '../../viewmodels/App';
 import Image from 'react-native-expo-cached-image';
+import { Ionicons } from '@expo/vector-icons';
+import Networks from '../../viewmodels/Networks';
 import { StatusBar } from 'expo-status-bar';
+import { formatAddress } from '../../utils/formatter';
+import icons from '../../assets/icons/crypto';
+import { observer } from 'mobx-react-lite';
 import { useHeaderHeight } from '@react-navigation/elements';
 import { useSafeAreaInsets } from 'react-native-safe-area-context';
 
-export default () => {
+export default observer(() => {
   const { top } = useSafeAreaInsets();
   const headerHeight = useHeaderHeight();
   const { currentWallet } = App;
   const { currentAccount } = currentWallet || {};
 
+  const { current } = Networks;
+
   useEffect(() => {
-    return () => {
-      RNStatusBar.setBarStyle('dark-content');
-    };
+    return () => {};
   }, []);
 
   return (
-    <View>
-      <View style={{ paddingTop: top + headerHeight, width: '100%', backgroundColor: 'dodgerblue' }}>
-        <Image />
+    <View style={{ backgroundColor: '#fff', flex: 1, paddingHorizontal: 16 }}>
+      <View
+        style={{
+          paddingTop: top + headerHeight,
+          backgroundColor: 'dodgerblue',
+          position: 'relative',
+          marginBottom: 48,
+          marginHorizontal: -16,
+        }}
+      >
+        <View
+          style={{
+            marginHorizontal: 16,
+            bottom: -37,
+            position: 'absolute',
+            borderRadius: 200,
+            padding: 0,
+            borderWidth: 3,
+            borderColor: '#fff',
+          }}
+        >
+          <Image
+            source={
+              currentWallet?.currentAccount?.avatar
+                ? { uri: currentWallet?.currentAccount?.avatar }
+                : icons[current.symbol.toLowerCase()]
+            }
+            style={{
+              width: 64,
+              height: 64,
+              borderRadius: 200,
+              backgroundColor: current.color,
+            }}
+          />
+        </View>
       </View>
 
+      <View>
+        <View style={{ flexDirection: 'row', alignItems: 'baseline', justifyContent: 'space-between' }}>
+          <View style={{ flexDirection: 'row', alignItems: 'baseline' }}>
+            <Text style={{ fontSize: 17, fontWeight: '500' }}>nick.eth</Text>
+            <Text style={{ marginStart: 8, fontSize: 15, color: secondaryFontColor }}>
+              {formatAddress('0x983110309620D911731Ac0932219af06091b6744', 7, 5)}
+            </Text>
+          </View>
+
+          <View style={{ flexDirection: 'row', alignItems: 'center', marginTop: 8 }}>
+            <Ionicons name="location" size={15} color="dodgerblue" style={{ marginEnd: 4 }} />
+            <Text style={{ fontSize: 14, color: 'dodgerblue' }}>USA</Text>
+          </View>
+        </View>
+
+        <Text style={{ lineHeight: 19, marginTop: 8, color: thirdFontColor }} numberOfLines={2}>
+          "If anyone would come after me, let him deny himself and take up his cross daily and follow me. For whoever would
+          save his life will lose it, but whoever loses his life for my sake will save it. For what does it profit a man if he
+          gains the whole world and loses or forfeits himself?" - Jesus, Luke 9.23-25
+        </Text>
+      </View>
+
+      <Text style={{ marginTop: 24, color: secondaryFontColor }}>Accounts</Text>
+
+      <View></View>
+      <StatusBar style="light" />
     </View>
   );
-};
+});
