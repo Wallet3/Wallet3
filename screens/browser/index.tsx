dee1ef5108afec470883fcbed5181aacc17104b7
UnsignedInt8 <unsignedint8@gmail.com>
2021-12-30 15:18:22 +0800

    [UI] Add loading progress, handle web url

--- screens/browser/index.tsx
+++ screens/browser/index.tsx
@@ -1,27 +1,41 @@
-import React, { useRef, useState } from 'react';
-import { TextInput, View } from 'react-native';
+import * as Linking from 'expo-linking';
 
+import { Dimensions, TextInput, View } from 'react-native';
+import React, { useRef, useState } from 'react';
+import { WebView, WebViewNavigation } from 'react-native-webview';
+
+import { Bar } from 'react-native-progress';
 import Constants from 'expo-constants';
 import Networks from '../../viewmodels/Networks';
-import { TextBox } from '../../components';
-import { WebView } from 'react-native-webview';
 import { borderColor } from '../../constants/styles';
 import { isURL } from '../../utils/url';
+import { observer } from 'mobx-react-lite';
 import { useSafeAreaInsets } from 'react-native-safe-area-context';
 
-export default (props) => {
+const ScreenWidth = Dimensions.get('window').width;
+
+export default observer(() => {
   const { top } = useSafeAreaInsets();
+  const { current } = Networks;
   const webview = useRef<WebView>(null);
+  const addrRef = useRef<TextInput>(null);
 
   const [loadingProgress, setLoadingProgress] = useState(10);
   const [isFocus, setFocus] = useState(false);
+  const [hostname, setHostname] = useState('');
+  const [webUrl, setWebUrl] = useState('');
   const [addr, setAddr] = useState('');
   const [uri, setUri] = useState<string>('');
-  const addrRef = useRef<TextInput>(null);
 
   const onAddrSubmit = () => {
     if (!isURL(addr)) return;
-    addr.toLowerCase().startsWith('http://') ? setUri(addr) : setUri(`http://${addr}`);
+    addr.toLowerCase().startsWith('http') ? setUri(addr) : setUri(`http://${addr}`);
+  };
+
+  const onNavigationStateChange = (event: WebViewNavigation) => {
+    if (!event.url) return;
+    setWebUrl(event.url);
+    setHostname(Linking.parse(event.url).hostname!);
   };
 
   const appName = `Wallet3/${Constants?.manifest?.version ?? '0.0.0'}`;
@@ -39,13 +53,13 @@ export default (props) => {
           selectTextOnFocus={true}
           onFocus={() => setFocus(true)}
           onBlur={() => setFocus(false)}
-          defaultValue={isFocus ? addr : undefined}
-          value={isFocus ? undefined : addr}
+          defaultValue={isFocus ? webUrl : undefined}
+          value={isFocus ? undefined : hostname}
           onChangeText={(t) => setAddr(t)}
           onSubmitEditing={() => onAddrSubmit()}
           style={{
             backgroundColor: isFocus ? '#fff' : '#f5f5f5',
-            fontSize: 17,
+            fontSize: 16,
             paddingHorizontal: 15,
             flex: 1,
             paddingVertical: 6,
@@ -56,15 +70,18 @@ export default (props) => {
           }}
         />
 
-        {loadingProgress > 0 && loadingProgress < 100 ? (
-          <View
+        {loadingProgress > 0 && loadingProgress < 1 ? (
+          <Bar
+            width={ScreenWidth}
+            color={current.color}
+            height={2}
+            borderWidth={0}
+            borderRadius={0}
+            progress={loadingProgress}
             style={{
-              height: 2,
-              backgroundColor: Networks.current.color,
               position: 'absolute',
               bottom: 0,
               marginHorizontal: -16,
-              width: `${loadingProgress}%`,
             }}
           />
         ) : undefined}
@@ -74,9 +91,10 @@ export default (props) => {
         ref={webview}
         applicationNameForUserAgent={appName}
         source={{ uri }}
-        onLoadProgress={({ nativeEvent }) => setLoadingProgress(nativeEvent.progress * 100)}
-        onLoadEnd={() => setLoadingProgress(100)}
+        onLoadProgress={({ nativeEvent }) => setLoadingProgress(nativeEvent.progress)}
+        onLoadEnd={() => setLoadingProgress(1)}
+        onNavigationStateChange={onNavigationStateChange}
       />
     </View>
   );
-};
+});
