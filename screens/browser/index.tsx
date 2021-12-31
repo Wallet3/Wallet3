a929cb38fc5ab6b05c523d21987eea58a5e331d6
UnsignedInt8 <unsignedint8@gmail.com>
2021-12-31 08:31:26 +0800

    [UI] Relayout address bar

--- screens/browser/index.tsx
+++ screens/browser/index.tsx
@@ -1,10 +1,11 @@
 import * as Linking from 'expo-linking';
 
-import { Dimensions, TextInput, TouchableOpacity, View } from 'react-native';
+import { Dimensions, Text, TextInput, TouchableOpacity, View } from 'react-native';
 import React, { useRef, useState } from 'react';
 import { WebView, WebViewNavigation } from 'react-native-webview';
 
 import { Bar } from 'react-native-progress';
+import CollapsibleView from '@eliav2/react-native-collapsible-view';
 import Constants from 'expo-constants';
 import { Ionicons } from '@expo/vector-icons';
 import Networks from '../../viewmodels/Networks';
@@ -20,7 +21,7 @@ export default observer(() => {
   const webview = useRef<WebView>(null);
   const addrRef = useRef<TextInput>(null);
 
-  const [loadingProgress, setLoadingProgress] = useState(10);
+  const [loadingProgress, setLoadingProgress] = useState(0);
   const [isFocus, setFocus] = useState(false);
   const [canGoBack, setCanGoBack] = useState(false);
   const [canGoForward, setCanGoForward] = useState(false);
@@ -49,58 +50,72 @@ export default observer(() => {
 
   return (
     <View style={{ backgroundColor: `#fff`, flex: 1, paddingTop: top, position: 'relative' }}>
-      <View
-        style={{
-          flexDirection: 'row',
-          marginHorizontal: 6,
-          paddingBottom: 8,
-          position: 'relative',
-          alignItems: 'center',
-        }}
-      >
-        <TouchableOpacity style={{ paddingHorizontal: 8 }} onPress={() => webview.current?.goBack()} disabled={!canGoBack}>
-          <Ionicons name="chevron-back-outline" size={17} color={canGoBack ? '#000' : 'lightgrey'} />
-        </TouchableOpacity>
-
-        <TouchableOpacity
-          style={{ paddingHorizontal: 8 }}
-          onPress={() => webview.current?.goForward()}
-          disabled={!canGoForward}
-        >
-          <Ionicons name="chevron-forward-outline" size={17} color={canGoForward ? '#000' : 'lightgrey'} />
-        </TouchableOpacity>
-
-        <TextInput
-          ref={addrRef}
-          autoCapitalize="none"
-          keyboardType="web-search"
-          placeholderTextColor="#dfdfdf"
-          autoCorrect={false}
-          placeholder="enter website address"
-          selectTextOnFocus={true}
-          onFocus={() => setFocus(true)}
-          onBlur={() => setFocus(false)}
-          defaultValue={isFocus ? webUrl : undefined}
-          value={isFocus ? undefined : hostname}
-          onChangeText={(t) => setAddr(t)}
-          onSubmitEditing={() => onAddrSubmit()}
+      <View style={{ position: 'relative', paddingBottom: 8 }}>
+        <View
           style={{
-            backgroundColor: isFocus ? '#fff' : '#f5f5f5',
-            fontSize: 16,
-            color: webUrl.startsWith('https') && !isFocus ? '#76B947' : undefined,
-            paddingHorizontal: 8,
-            flex: 1,
-            paddingVertical: 6,
-            borderWidth: 1,
-            borderColor: isFocus ? borderColor : 'transparent',
-            borderRadius: 7,
-            textAlign: isFocus ? 'auto' : 'center',
+            flexDirection: 'row',
+            marginHorizontal: 6,
+            position: 'relative',
+            alignItems: 'center',
           }}
-        />
+        >
+          <TouchableOpacity style={{ paddingHorizontal: 8 }} onPress={() => webview.current?.goBack()} disabled={!canGoBack}>
+            <Ionicons name="chevron-back-outline" size={17} color={canGoBack ? '#000' : 'lightgrey'} />
+          </TouchableOpacity>
 
-        <TouchableOpacity style={{ paddingHorizontal: 8 }}>
-          <Ionicons name="home-outline" size={17} />
-        </TouchableOpacity>
+          <TouchableOpacity
+            style={{ paddingHorizontal: 8 }}
+            onPress={() => webview.current?.goForward()}
+            disabled={!canGoForward}
+          >
+            <Ionicons name="chevron-forward-outline" size={17} color={canGoForward ? '#000' : 'lightgrey'} />
+          </TouchableOpacity>
+
+          <TextInput
+            ref={addrRef}
+            autoCapitalize="none"
+            keyboardType="web-search"
+            placeholderTextColor="#dfdfdf"
+            autoCorrect={false}
+            placeholder="enter website address"
+            selectTextOnFocus={true}
+            onFocus={() => setFocus(true)}
+            onBlur={() => setFocus(false)}
+            defaultValue={isFocus ? webUrl : undefined}
+            value={isFocus ? undefined : hostname}
+            onChangeText={(t) => setAddr(t)}
+            onSubmitEditing={() => onAddrSubmit()}
+            style={{
+              backgroundColor: isFocus ? '#fff' : '#f5f5f5',
+              fontSize: 16,
+              color: webUrl.startsWith('https') && !isFocus ? '#76B947' : undefined,
+              paddingHorizontal: 8,
+              flex: 1,
+              paddingVertical: 6,
+              borderWidth: 1,
+              borderColor: isFocus ? borderColor : 'transparent',
+              borderRadius: 7,
+              textAlign: isFocus ? 'auto' : 'center',
+            }}
+          />
+
+          <TouchableOpacity style={{ paddingHorizontal: 8 }}>
+            <Ionicons name="bookmark-outline" size={17} />
+          </TouchableOpacity>
+        </View>
+
+        {uri ? (
+          <CollapsibleView
+            noArrow
+            arrowStyling={{ display: 'none' }}
+            titleStyle={{ display: 'none' }}
+            expanded={isFocus}
+            style={{ borderWidth: 0, padding: 0, paddingHorizontal: 6 }}
+            collapsibleContainerStyle={{}}
+          >
+            <Text>hey there!</Text>
+          </CollapsibleView>
+        ) : undefined}
 
         {loadingProgress > 0 && loadingProgress < 1 ? (
           <Bar
@@ -113,7 +128,6 @@ export default observer(() => {
             style={{
               position: 'absolute',
               bottom: 0,
-              marginHorizontal: -16,
             }}
           />
         ) : undefined}
