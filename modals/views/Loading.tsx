8054a7b2ae55f42883bf1ae7fbc7e0ec5cdee031
UnsignedInt8 <unsignedint8@gmail.com>
2022-01-17 14:35:29 +0800

    [UI] Fix animation not auto play

--- modals/views/Loading.tsx
+++ modals/views/Loading.tsx
@@ -1,11 +1,23 @@
+import React, { useEffect, useRef } from 'react';
+
 import LottieView from 'lottie-react-native';
-import React from 'react';
 import { View } from 'react-native';
 
 export default () => {
+  const ref = useRef<LottieView>(null);
+
+  useEffect(() => {
+    ref?.current?.play();
+  }, []);
+
   return (
     <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-      <LottieView autoPlay duration={3000} style={{ width: 230, height: 230 }} source={require('../../assets/animations/loading.json')} />
+      <LottieView
+        ref={ref}
+        duration={3000}
+        style={{ width: 230, height: 230 }}
+        source={require('../../assets/animations/loading.json')}
+      />
     </View>
   );
 };
