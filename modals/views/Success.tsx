ab436ab9a8f58c71b67cf67a134525c64dcc0cad
UnsignedInt8 <unsignedint8@gmail.com>
2022-01-17 14:31:39 +0800

    [UI] Fix animation not autoPlay on pad

--- modals/views/Success.tsx
+++ modals/views/Success.tsx
@@ -1,20 +1,27 @@
+import React, { useEffect, useRef } from 'react';
+
 import LottieView from 'lottie-react-native';
-import React from 'react';
 import { View } from 'react-native';
 
 export default () => {
+  const ref = useRef<LottieView>(null);
+
+  useEffect(() => {
+    ref.current?.play();
+  }, []);
+
   return (
     <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
       <LottieView
+        ref={ref}
+        loop={false}
+        source={require('../../assets/animations/success.json')}
         style={{
           width: 200,
           height: 200,
           justifyContent: 'center',
           alignItems: 'center',
         }}
-        loop={false}
-        autoPlay
-        source={require('../../assets/animations/success.json')}
       />
     </View>
   );
