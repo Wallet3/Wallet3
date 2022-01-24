ea2ccd7bb97fd6d9fde96f40fced0d462238ea05
UnsignedInt8 <unsignedint8@gmail.com>
2022-01-24 16:46:08 +0800

    [UI] Add scan button for importing wallet

--- modals/multi-accounts/ImportWallet.tsx
+++ modals/multi-accounts/ImportWallet.tsx
@@ -1,14 +1,17 @@
 import { Button, Loader, SafeViewContainer } from '../../components';
-import React, { useEffect, useState } from 'react';
-import { Text, TextInput, View } from 'react-native';
+import React, { useEffect, useRef, useState } from 'react';
+import { Text, TextInput, TouchableOpacity, View } from 'react-native';
 import { borderColor, secondaryFontColor } from '../../constants/styles';
 
 import App from '../../viewmodels/App';
+import { MaterialCommunityIcons } from '@expo/vector-icons';
 import { MnemonicOnce } from '../../viewmodels/MnemonicOnce';
 import Networks from '../../viewmodels/Networks';
 import RejectApproveButtons from '../components/RejectApproveButtons';
+import { decode } from 'js-base64';
 import i18n from '../../i18n';
 import { showMessage } from 'react-native-flash-message';
+import { useNavigation } from '@react-navigation/native';
 
 export default ({ onDone, onCancel }: { onDone?: () => void; onCancel?: () => void }) => {
   const { t } = i18n;
@@ -17,6 +20,7 @@ export default ({ onDone, onCancel }: { onDone?: () => void; onCancel?: () => vo
   const [secret, setSecret] = useState('');
   const [verified, setVerified] = useState(false);
   const [busy, setBusy] = useState(false);
+  const [defaultSecret, setDefaultSecret] = useState<string>();
 
   useEffect(() => setVerified(mnemonic.setSecret(secret)), [secret]);
 
@@ -39,27 +43,52 @@ export default ({ onDone, onCancel }: { onDone?: () => void; onCancel?: () => vo
     }, 0);
   };
 
+  useEffect(() => {
+    PubSub.subscribe('CodeScan-wallet3sync:', (_, { data }: { data: string }) => {
+      const encoded = data.substring(12);
+      const decoded = decode(encoded).replaceAll(',', ' ').trim();
+
+      if (!mnemonic.setSecret(decoded)) return;
+      setDefaultSecret(decoded);
+    });
+
+    return () => {
+      PubSub.unsubscribe('CodeScan-wallet3sync:');
+    };
+  }, []);
+
   return (
     <SafeViewContainer style={{ padding: 16 }}>
-      <TextInput
-        multiline={true}
-        numberOfLines={5}
-        placeholder={t('land-import-placeholder')}
-        onChangeText={(txt) => setSecret(txt)}
-        autoCapitalize="none"
-        keyboardType="default"
-        secureTextEntry={true}
-        style={{
-          height: 150,
-          textAlignVertical: 'top',
-          borderWidth: 1,
-          borderColor: themeColor,
-          borderRadius: 10,
-          padding: 8,
-          paddingVertical: 24,
-          fontSize: 16,
-        }}
-      />
+      <View style={{ position: 'relative' }}>
+        <TextInput
+          multiline={true}
+          numberOfLines={5}
+          placeholder={t('land-import-placeholder')}
+          onChangeText={(txt) => setSecret(txt)}
+          defaultValue={defaultSecret}
+          autoCapitalize="none"
+          keyboardType="default"
+          secureTextEntry={true}
+          style={{
+            height: 150,
+            textAlignVertical: 'top',
+            borderWidth: 1,
+            borderColor: themeColor,
+            borderRadius: 10,
+            padding: 8,
+            paddingVertical: 24,
+            fontSize: 16,
+          }}
+        />
+
+        <TouchableOpacity
+          style={{ position: 'absolute', bottom: 0, right: 0, padding: 10, alignItems: 'center', justifyContent: 'center' }}
+          // onPress={() => }
+        >
+          <MaterialCommunityIcons name="scan-helper" size={16} color={themeColor} />
+          <View style={{ height: 1, position: 'absolute', width: 14, backgroundColor: themeColor }} />
+        </TouchableOpacity>
+      </View>
 
       <View
         style={{
