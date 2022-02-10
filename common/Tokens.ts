7ddface5881fda11d2ea4853166ae95000e559fe
UnsignedInt8 <unsignedint8@gmail.com>
2022-02-10 19:39:53 +0800

    [UI] Add aurora tokens

--- common/Tokens.ts
+++ common/Tokens.ts
@@ -625,8 +625,36 @@ const Boba_DAI = {
 export const BobaPopularTokens = [Boba_DAI, Boba_USDC, Boba_USDT];
 
 const Aurora_DAI = {
-  address: '',
+  address: '0xe3520349F477A5F6EB06107066048508498A291b',
+  decimals: 18,
+  symbol: 'DAI',
 };
+
+const Aurora_BUSD = {
+  address: '0x5C92A4A7f59A9484AFD79DbE251AD2380E589783',
+  decimals: 18,
+  symbol: 'BUSD',
+};
+
+const Aurora_USDT = {
+  address: '0x4988a896b1227218e4A686fdE5EabdcAbd91571f',
+  decimals: 18,
+  symbol: 'USDT',
+};
+
+const Aurora_USDC = {
+  address: '0xB12BFcA5A55806AaF64E99521918A4bf0fC40802',
+  decimals: 6,
+  symbol: 'USDC',
+};
+
+const Aurora_AURORA = {
+  address: '0x8BEc47865aDe3B172A928df8f990Bc7f2A3b9f79',
+  decimals: 18,
+  symbol: 'AURORA',
+};
+
+export const AuroraPopularTokens = [Aurora_AURORA, Aurora_DAI, Aurora_USDC, Aurora_USDT, Aurora_BUSD];
 
 const Ronin_AXS = {
   address: '0x97a9107C1793BC407d6F527b77e7fff4D812bece',
