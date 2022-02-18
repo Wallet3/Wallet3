8cc775a7cdf4fc1e11b58e566f3cf02824985113
UnsignedInt8 <unsignedint8@gmail.com>
2022-02-18 10:44:25 +0800

    [UI] Fix max width

--- modals/dapp/RequestReview.tsx
+++ modals/dapp/RequestReview.tsx
@@ -65,7 +65,7 @@ const TxReview = observer(({ vm, onReject, onApprove, onGasPress, app, account, 
 
         <View style={{ ...reviewItemStyle }}>
           <Text style={styles.reviewItemTitle}>{t('modal-dapp-request-type')}</Text>
-          <Text style={{ ...reviewItemValueStyle }} numberOfLines={1}>
+          <Text style={{ ...reviewItemValueStyle, maxWidth: '70%' }} numberOfLines={1}>
             {t(`tx-type-${vm.type.toLowerCase().replace(' ', '-')}`)}
           </Text>
         </View>
