0a1d95ab0614d6821f4ccf92cbf8fa2a7ad417a8
UnsignedInt8 <unsignedint8@gmail.com>
2022-02-26 19:11:27 +0800

    [UI] Remove json tree

--- modals/dapp/SignTypedData.tsx
+++ modals/dapp/SignTypedData.tsx
@@ -1,6 +1,5 @@
-import { Button, SafeViewContainer } from '../../components';
 import { Entypo, MaterialCommunityIcons } from '@expo/vector-icons';
-import React, { useEffect, useState } from 'react';
+import React, { useState } from 'react';
 import { Text, TouchableOpacity, View } from 'react-native';
 
 import { Account } from '../../viewmodels/account/Account';
@@ -8,35 +7,12 @@ import AccountIndicator from '../components/AccountIndicator';
 import { BioType } from '../../viewmodels/Authentication';
 import Collapsible from 'react-native-collapsible';
 import FaceID from '../../assets/icons/app/FaceID-white.svg';
-import JSONTree from 'react-native-json-tree';
 import RejectApproveButtons from '../components/RejectApproveButtons';
+import { SafeViewContainer } from '../../components';
 import { ScrollView } from 'react-native-gesture-handler';
 import Theme from '../../viewmodels/settings/Theme';
-import Themes from '../../screens/settings/Themes';
-import { borderColor } from '../../constants/styles';
 import i18n from '../../i18n';
 import { observer } from 'mobx-react-lite';
-
-const LightJsonTheme = {
-  scheme: 'google',
-  author: 'seth wright (http://sethawright.com)',
-  base00: '#1d1f21',
-  base01: '#282a2e',
-  base02: '#373b41',
-  base03: '#969896',
-  base04: '#b4b7b4',
-  base05: '#c5c8c6',
-  base06: '#e0e0e0',
-  base07: '#ffffff',
-  base08: '#CC342B',
-  base09: '#F96A38',
-  base0A: '#FBA922',
-  base0B: '#198844',
-  base0C: '#3971ED',
-  base0D: '#3971ED',
-  base0E: '#A36AC7',
-  base0F: '#3971ED',
-};
 
 interface Props {
   themeColor: string;
@@ -68,7 +44,10 @@ const parse = (obj: any) => {
 
 const generateCollapsibleItem = ({ item }: { item: { key: string; value: any[] } }) => {
   const [collapsed, setCollapsed] = useState(true);
-  const { textColor } = Theme;
+  const { textColor, thirdTextColor } = Theme;
+  const { t } = i18n;
+
+  if (!item) return null;
 
   return (
     <View key={item.key}>
@@ -81,7 +60,9 @@ const generateCollapsibleItem = ({ item }: { item: { key: string; value: any[] }
         </Text>
 
         <View style={{ flexDirection: 'row', alignItems: 'center' }}>
-          <Text style={{ fontSize: 12, color: textColor }} numberOfLines={1}>{`Items: ${item.value.length}`}</Text>
+          <Text style={{ fontSize: 12, color: thirdTextColor, opacity: 0.5 }} numberOfLines={1}>
+            {t('modal-sign-typed-msg-items', { count: item.value.length })}
+          </Text>
           <Entypo
             name={collapsed ? 'chevron-down' : 'chevron-up'}
             color={textColor}
@@ -136,8 +117,6 @@ export default observer(({ themeColor, data, onReject, onSign, account, bioType 
       : () => <MaterialCommunityIcons name="fingerprint" size={19} color="#fff" />
     : undefined;
 
-  console.log('parse:\n', parse(data.message));
-
   return (
     <SafeViewContainer style={{ flex: 1 }}>
       <View
@@ -156,8 +135,7 @@ export default observer(({ themeColor, data, onReject, onSign, account, bioType 
       </View>
 
       <ScrollView style={{ flex: 1, marginHorizontal: -16, paddingHorizontal: 12 }} bounces={false}>
-        {/* <JSONTree data={data} hideRoot theme={isLightMode ? LightJsonTheme : undefined} /> */}
-        {generateItem({ data: parse(data.message)! })}
+        {data?.message ? generateItem({ data: parse(data.message)! }) : generateItem({ data: parse(data)! })}
       </ScrollView>
 
       <RejectApproveButtons
