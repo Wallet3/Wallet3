c01abb2f5ccf1a6e00c4f18d730d7bd37d33efd3
UnsignedInt8 <unsignedint8@gmail.com>
2022-04-03 20:19:22 +0800

    [UI] Fixed polyonscan icon and link

--- assets/3rd/index.ts
+++ assets/3rd/index.ts
@@ -3,8 +3,9 @@ import Etherscan from './etherscan-logo-circle.svg';
 import Github from './github.svg';
 import Metamask from './metamask.svg';
 import Opensea from './opensea.svg';
+import Polygon from './polygon.svg';
 import Rarible from './rarible.svg';
 import Twitter from './twitter.svg';
 import WalletConnect from './walletconnect.svg';
 
-export { Twitter, Github, Opensea, Discord, Metamask, WalletConnect, Etherscan, Rarible };
+export { Twitter, Github, Opensea, Discord, Metamask, WalletConnect, Etherscan, Rarible, Polygon };
