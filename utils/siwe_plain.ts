8548bc0cb2c1fb95a4e44a96af3061a8e320d2b4
UnsignedInt8 <unsignedint8@gmail.com>
2022-04-24 09:16:46 +0800

    [VM] Remove regex

--- utils/siwe_plain.ts
+++ utils/siwe_plain.ts
@@ -1,19 +1,20 @@
 import { isURL } from './url';
+import { utils } from 'ethers';
 
-const DOMAIN = '(([^?#]*)) wants you to sign in with your Ethereum account:';
+const DOMAIN = 'wants you to sign in with your Ethereum account:';
 const ADDRESS = '0x[a-zA-Z0-9]{40}';
 const STATEMENT = '(([^\\n]+)\\n)?';
 const URI = '(([^:?#]+):)?(([^?#]*))?([^?#]*)(\\?([^#]*))?(#(.*))';
-const URI_LINE = `URI: (${URI}?)`;
-const VERSION = 'Version: 1';
-const CHAIN_ID = 'Chain ID: [0-9]+';
-const NONCE = 'Nonce: [a-zA-Z0-9]{8,}';
+const URI_LINE = `URI: `;
+const VERSION = 'Version: ';
+const CHAIN_ID = 'Chain ID: ';
+const NONCE = 'Nonce: ';
 const DATETIME = `([0-9]+)-(0[1-9]|1[012])-(0[1-9]|[12][0-9]|3[01])[Tt]([01][0-9]|2[0-3]):([0-5][0-9]):([0-5][0-9]|60)(\.[0-9]+)?(([Zz])|([\+|\-]([01][0-9]|2[0-3]):[0-5][0-9]))`;
-const ISSUED_AT = `Issued At: ${DATETIME}`;
-const EXPIRATION_TIME = `Expiration Time: ${DATETIME}?`;
-const NOT_BEFORE = `Not Before: ${DATETIME}?`;
-const REQUEST_ID = "Request ID: [-._~!$&'()*+,;=:@%a-zA-Z0-9]*?";
-const RESOURCES = `Resources:\\n- ${URI}+?`;
+const ISSUED_AT = `Issued At: `;
+const EXPIRATION_TIME = `Expiration Time: `;
+const NOT_BEFORE = `Not Before: `;
+const REQUEST_ID = 'Request ID: ';
+const RESOURCES = `Resources:`;
 
 export class ParsedMessage {
   domain: string | undefined;
@@ -27,42 +28,36 @@ export class ParsedMessage {
   expirationTime: string | null | undefined;
   notBefore: string | null | undefined;
   requestId: string | null | undefined;
-  resources: string[] | null | undefined;
 
   constructor(msg: string) {
     const components = msg?.split('\n').filter((i) => i);
 
-    const domains = new RegExp(DOMAIN).exec(components[0])?.filter((i) => i) || [];
-    this.domain = domains[domains?.length - 1] || '';
+    this.domain = components[0]?.endsWith(DOMAIN) ? components[0].split(' ')[0] : '';
     if (!isURL(this.domain)) throw new Error('Invalid domain');
 
-    this.address = new RegExp(ADDRESS).test(components[1] || '') ? components[1] : undefined;
+    this.address = utils.isAddress(components[1]) ? components[1] : '';
     if (!this.address) throw new Error('Invalid address');
 
-    this.statement = new RegExp(STATEMENT).test(components[2] || '') ? components[2] : undefined;
+    this.statement = components[2] || '';
 
-    this.uri = new RegExp(URI_LINE).test(components[3] || '') ? components[3]?.split(': ')[1] : undefined;
-    if (!isURL(this.uri || '')) throw new Error('Invalid URI');
+    this.uri = components[3]?.startsWith(URI_LINE) ? components[3].split(': ')[1] : '';
+    if (!isURL(this.uri)) throw new Error('Invalid URI');
 
-    this.version = new RegExp(VERSION).test(components[4] || '') ? Number(components[4]?.split(': ')[1]) : undefined;
+    this.version = components[4]?.startsWith(VERSION) ? Number(components[4]?.split(': ')[1]) : undefined;
     if (!this.version) throw new Error('Invalid version');
 
-    this.chainId = Number(new RegExp(CHAIN_ID).test(components[5] || '') ? Number(components[5]?.split(': ')[1]) : undefined);
+    this.chainId = components[5]?.startsWith(CHAIN_ID) ? Number(components[5]?.split(': ')[1]) : undefined;
     if (!this.chainId) throw new Error('Invalid chainID');
 
-    this.nonce = new RegExp(NONCE).test(components[6] || '') ? components[6]?.split(': ')[1] : undefined;
+    this.nonce = components[6]?.startsWith(NONCE) ? components[6]?.split(': ')[1] : undefined;
     if (!this.nonce) throw new Error('Invalid nonce');
 
-    this.issuedAt = new RegExp(ISSUED_AT).test(components[7] || '') ? components[7]?.split(': ')[1] : undefined;
+    this.issuedAt = components[7]?.startsWith(ISSUED_AT) ? components[7]?.split(': ')[1] : undefined;
 
-    this.expirationTime = new RegExp(EXPIRATION_TIME).test(components[8]) ? components[8]?.split(': ')[1] : undefined;
+    this.expirationTime = components[8]?.startsWith(EXPIRATION_TIME) ? components[8]?.split(': ')[1] : undefined;
 
-    this.notBefore = new RegExp(NOT_BEFORE).test(components[9] || '') ? components[9]?.split(': ')[1] : undefined;
+    this.notBefore = components[9]?.startsWith(NOT_BEFORE) ? components[9]?.split(': ')[1] : undefined;
 
-    this.requestId = new RegExp(REQUEST_ID).test(components[10] || '') ? components[10]?.split(': ')[1] : undefined;
-
-    this.resources = new RegExp(RESOURCES).test(components[11] || '')
-      ? components[11]?.split(': ')[1]?.split?.('\n- ')
-      : undefined;
+    this.requestId = components[10]?.startsWith(REQUEST_ID) ? components[10]?.split(': ')[1] : undefined;
   }
 }
