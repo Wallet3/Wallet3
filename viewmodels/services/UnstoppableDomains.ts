93096eb075be7b55efee8a0c27fc084e8eeaa53e
UnsignedInt8 <unsignedint8@gmail.com>
2022-08-20 10:35:57 +0900

    [VM] Support more suffixes

--- viewmodels/services/UnstoppableDomains.ts
+++ viewmodels/services/UnstoppableDomains.ts
@@ -1,10 +1,13 @@
 import { getRecords } from '../../common/apis/UnstoppableDomains';
 
+const unDomains = ['.crypto', '.nft', '.x', '.wallet', '.bitcoin', '.dao', '.888', '.zil', '.blockchain'];
+
 export async function resolveDomain(domain: string) {
   const records = await getRecords(domain);
   return records?.records?.['crypto.ETH.address'] ?? '';
 }
 
 export function isUnstoppableDomains(domain: string) {
-  return domain.endsWith('.nft') || domain.endsWith('.crypto');
+  const lower = domain.toLowerCase();
+  return unDomains.some((item) => lower.endsWith(item));
 }
