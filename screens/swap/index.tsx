4a83ccd356c910f0501d6f082f7b01a7ccac592f
Ambince <1657846341@qq.com>
2022-08-26 17:19:03 +0800

    [UI]  swap UI

--- screens/swap/index.tsx
+++ screens/swap/index.tsx
@@ -1,10 +1,7 @@
-import React from 'react';
+import React, { useRef } from 'react';
 import { observer } from 'mobx-react-lite';
-import { useEffect } from 'react';
-import { useRef } from 'react';
 import { utils } from 'ethers';
-import { Feather } from '@expo/vector-icons';
-import { Text, Button, ListRenderItemInfo, Pressable, TextInput, View, TouchableOpacity } from 'react-native';
+import { Button, ListRenderItemInfo, StyleSheet, Text, TextInput, TouchableOpacity, View } from 'react-native';
 import { IToken } from '../../common/tokens';
 import Theme from '../../viewmodels/settings/Theme';
 import i18n from '../../i18n';
@@ -12,12 +9,8 @@ import Networks from '../../viewmodels/Networks';
 import App from '../../viewmodels/App';
 import { DrawerScreenProps } from '@react-navigation/drawer';
 import Swap from '../../viewmodels/Swap';
-import { color } from 'react-native-reanimated';
-import { generateNetworkIcon } from '../../assets/icons/networks/color';
-import styles from '../land/styles';
-import ContextMenu from 'react-native-context-menu-view';
 import { INetwork } from '../../common/Networks';
-import { title } from 'process';
+import { Feather } from '@expo/vector-icons';
 
 type SwapStack = {
   Swap: undefined;
@@ -42,7 +35,7 @@ export default observer(({ navigation }: DrawerScreenProps<SwapStack, 'Swap'>) =
   const TokenMenu = ({
     selectedToken,
     tokens,
-    onTokenSelected,
+    onTokenSelected
   }: {
     selectedToken: IToken;
     tokens: IToken[];
@@ -77,143 +70,134 @@ export default observer(({ navigation }: DrawerScreenProps<SwapStack, 'Swap'>) =
 
   return (
     <View
-      style={{
+      style={ {
         flex: 1,
         padding: 16,
         paddingBottom: 0,
         alignItems: 'stretch',
         justifyContent: 'flex-start',
-        backgroundColor,
-      }}
+        backgroundColor
+      } }
     >
+
+
+      <View style={ styles.swapbox }>
+        <TouchableOpacity
+          onPress={ () => {
+            const value = utils.formatUnits(Swap.max, Swap.from?.decimals);
+            Swap.setFromAmount(value);
+          } }
+        >
+          <Text style={ styles.max }>Max: { utils.formatUnits(Swap.max, Swap.from?.decimals || 0) }</Text>
+        </TouchableOpacity>
+        <TextInput
+          style={ styles.from }
+          numberOfLines={ 1 }
+          autoFocus
+          defaultValue='0.00'
+          value={ Swap.fromAmount }
+          onChangeText={ (t) => Swap.setFromAmount(t) }
+          autoCorrect={ false }
+        />
+        <View style={ { flexDirection: 'row', alignItems: 'center', justifyContent: 'flex-end' } }>
+          <TokenMenu selectedToken={ Swap.from! } tokens={ Swap.fromList } onTokenSelected={ (t) => Swap.selectFrom(t) } />
+        </View>
+      </View>
+
+
       <TouchableOpacity
-        onPress={() => {
-          const value = utils.formatUnits(Swap.max, Swap.from?.decimals);
-          Swap.setFromAmount(value);
-        }}
+        activeOpacity={1}
+        style={ styles.arrow }
+        onPress={ (_) => {
+          Swap.interchange();
+          Swap.fromAmount = '';
+        } }
       >
-        <Text>Max: {utils.formatUnits(Swap.max, Swap.from?.decimals || 0)}</Text>
+        <Feather name='arrow-down' style={ styles.svg } />
       </TouchableOpacity>
 
-      <View style={{ flexDirection: 'row' }}>
-        <TextInput
-          numberOfLines={1}
-          autoFocus
-          defaultValue="0.00"
-          value={Swap.fromAmount}
-          onChangeText={(t) => Swap.setFromAmount(t)}
-          autoCorrect={false}
-        />
-        <View style={{ flexDirection: 'row', alignItems: 'center', justifyContent: 'flex-end' }}>
-          <TokenMenu selectedToken={Swap.from!} tokens={Swap.fromList} onTokenSelected={(t) => Swap.selectFrom(t)} />
+      <View style={ styles.swapbox }>
+        <TextInput style={ styles.from } defaultValue='0.00' numberOfLines={ 1 } editable={ false } value={ Swap.forAmount } autoCorrect={ false } />
+        <View style={ { flexDirection: 'row', alignItems: 'center', justifyContent: 'flex-end' } }>
+          <TokenMenu selectedToken={ Swap.for! } tokens={ Swap.forList } onTokenSelected={ (t) => Swap.selectFor(t) } />
         </View>
       </View>
 
-      <View style={{ marginTop: 24, flexDirection: 'row' }}>
-        <TextInput defaultValue="0.00" numberOfLines={1} editable={false} value={Swap.forAmount} autoCorrect={false} />
-        <View style={{ flexDirection: 'row', alignItems: 'center', justifyContent: 'flex-end' }}>
-          <TokenMenu selectedToken={Swap.for!} tokens={Swap.forList} onTokenSelected={(t) => Swap.selectFor(t)} />
-        </View>
-      </View>
-
-      <View style={{ marginVertical: 24, flexDirection: 'row', justifyContent: 'space-between' }}>
-        <View style={{ flexDirection: 'row', alignItems: 'center', justifyContent: 'flex-end' }}>
-          <Text>{t('slippage')}:</Text>
-          <TouchableOpacity onPress={() => Swap.setSlippage(0.5)}>
-            <Text style={{ padding: 4, color: Swap.slippage === 0.5 ? 'blue' : 'lightblue' }}>{'0.5%'}</Text>
+      <View style={ { marginVertical: 24, flexDirection: 'row', justifyContent: 'space-between' } }>
+        <View style={ { flexDirection: 'row', alignItems: 'center', justifyContent: 'flex-end' } }>
+          <Text>{ t('slippage') }:</Text>
+          <TouchableOpacity onPress={ () => Swap.setSlippage(0.5) }>
+            <Text style={ { padding: 4, color: Swap.slippage === 0.5 ? 'blue' : 'lightblue' } }>{ '0.5%' }</Text>
           </TouchableOpacity>
-          <TouchableOpacity onPress={() => Swap.setSlippage(1)}>
-            <Text style={{ padding: 4, color: Swap.slippage === 1 ? 'blue' : 'lightblue' }}>{'1%'}</Text>
+          <TouchableOpacity onPress={ () => Swap.setSlippage(1) }>
+            <Text style={ { padding: 4, color: Swap.slippage === 1 ? 'blue' : 'lightblue' } }>{ '1%' }</Text>
           </TouchableOpacity>
-          <TouchableOpacity onPress={() => Swap.setSlippage(2)}>
-            <Text style={{ padding: 4, color: Swap.slippage === 2 ? 'blue' : 'lightblue' }}>{'2%'}</Text>
+          <TouchableOpacity onPress={ () => Swap.setSlippage(2) }>
+            <Text style={ { padding: 4, color: Swap.slippage === 2 ? 'blue' : 'lightblue' } }>{ '2%' }</Text>
           </TouchableOpacity>
         </View>
 
         <Text>
-          {t('modal-review-fee')}: {Swap.fee}%
+          { t('modal-review-fee') }: { Swap.fee }%
         </Text>
       </View>
 
-      {/* <div className="swap">
-          <div className="max">
-            <span
-              onClick={(_) => {
-                const value = utils.formatUnits(Swap.max, Swap.from?.decimals);
-                Swap.setFromAmount(value);
-                fromAmountRef.current.value = value;
-              }}
-            >
-              Max: {utils.formatUnits(Swap.max, Swap.from?.decimals || 0)}
-            </span>
-          </div>
-
-          <div className="swapbox from">
-            <input
-              ref={fromAmountRef}
-              type="text"
-              autoFocus
-              placeholder="0.00"
-              onChange={(e) => Swap.setFromAmount(e.target.value)}
-            />
-
-            {Swap.fromList.length > 0 ? (
-              <TokenMenu selectedToken={Swap.from} tokens={Swap.fromList} onTokenSelected={(t) => Swap.selectFrom(t)} />
-            ) : (
-              <div className="empty-menu-placeholder" />
-            )}
-          </div>
-
-          <div
-            className="arrow"
-            onClick={(_) => {
-              Swap.interchange();
-              fromAmountRef.current.value = '';
-            }}
-          >
-            <Feather icon="arrow-down" size={12} />
-          </div>
-
-          <div className="swapbox">
-            <input type="text" readOnly value={Swap.forAmount} />
-
-            {Swap.forList.length > 0 ? (
-              <TokenMenu selectedToken={Swap.for} tokens={Swap.forList} onTokenSelected={(t) => Swap.selectFor(t)} />
-            ) : (
-              <div className="empty-menu-placeholder" />
-            )}
-          </div>
-
-          <div className="info">
-            <div className="slippages">
-              {t('Slippage')}:
-              <span className={`${Swap.slippage === 0.5 ? 'active' : ''}`} onClick={(_) => Swap.setSlippage(0.5)}>
-                0.5%
-              </span>
-              <span className={`${Swap.slippage === 1 ? 'active' : ''}`} onClick={(_) => Swap.setSlippage(1)}>
-                1%
-              </span>
-              <span className={`${Swap.slippage === 2 ? 'active' : ''}`} onClick={(_) => Swap.setSlippage(2)}>
-                2%
-              </span>
-            </div>
-            <span>
-              {t('Fee')}: {Swap.fee}%
-            </span>
-          </div>
-        </div> */}
-
-      {!Swap.approved ? (
+      { !Swap.approved ? (
         <Button
-          title={t('tx-type-approve')}
-          disabled={!Swap.fromAmount || Swap.approving || Swap.fromList.length === 0}
-          onPress={() => Swap.approve()}
+          title={ t('tx-type-approve') }
+          disabled={ !Swap.fromAmount || Swap.approving || Swap.fromList.length === 0 }
+          onPress={ () => Swap.approve() }
         />
-      ) : undefined}
+      ) : undefined }
 
-      {Swap.approved ? (
-        <Button title={t('home-tab-swap')} disabled={!Swap.isValid || Swap.swapping} onPress={() => Swap.swap()} />
-      ) : undefined}
+      { Swap.approved ? (
+        <Button title={ t('home-tab-swap') } disabled={ !Swap.isValid || Swap.swapping } onPress={ () => Swap.swap() } />
+      ) : undefined }
     </View>
   );
 });
+
+
+const styles = StyleSheet.create({
+  swapbox: {
+    flexDirection: 'column',
+    display: 'flex',
+    fontSize: 10,
+    borderRadius: 7,
+    paddingVertical: 6,
+    paddingHorizontal: 10,
+    borderWidth: 1,
+    borderStyle: 'solid',
+    borderColor: '#dfe8f9'
+  },
+
+  max: {
+    fontSize: 10,
+    marginBottom: -18
+  },
+
+  from: {
+    paddingTop: 20
+  },
+  arrow: {
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+    zIndex: 9,
+    backgroundColor: 'white'
+  },
+  svg: {
+    height: 24,
+    width: 24,
+    padding: 6,
+    margin: -5,
+    borderRadius: 12,
+    borderWidth: 1,
+    borderStyle: 'solid',
+    borderColor: '#dfe8f9',
+    backgroundColor: 'aliceblue'
+  }
+
+
+});
+
