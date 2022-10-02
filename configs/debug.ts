099f82083aa3cda533409573897a0aa9763eb11a
UnsignedInt8 <unsignedint8@gmail.com>
2022-10-02 15:49:31 +0900

    [Project] 3.2.0 Release

--- configs/debug.ts
+++ configs/debug.ts
@@ -3,13 +3,11 @@ import { LogBox } from 'react-native';
 LogBox.ignoreLogs([
   'ReactNativeFiberHostComponent: Calling getNode() on the ref of an Animated component is no longer necessary. You can now directly use the ref instead. This method will be removed in a future release.',
   'VirtualizedLists should never be nested inside plain ScrollViews with the same orientation because it can break windowing and other functionality - use another VirtualizedList-backed container instead.',
-  'componentWillUpdate has been renamed, and is not recommended for use. See https://reactjs.org/link/unsafe-component-lifecycles for details.',
   'Face ID is not available in Expo Go. You can use it in a standalone Expo app by providing `NSFaceIDUsageDescription`.',
   '[mobx] Out of bounds read',
   'Webview Process Terminated',
   'source.uri should not be an empty string',
   'check out new Gestures system',
   'Could not find image file',
-  'react-native-view-shot: NativeModules.RNViewShot is undefined.',
   'Require cycle:'
 ]);
