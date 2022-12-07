bdb59257f5788bd7a841e9ee6ed0ea50caa74b6e
UnsignedInt8 <unsignedint8@gmail.com>
2022-12-07 21:05:34 +0900

    [UI] Added Canto white icon

--- screens/browser/scripts/InjectRainbowKitObserver.ts
+++ screens/browser/scripts/InjectRainbowKitObserver.ts
@@ -6,9 +6,9 @@ const observerRainbowKit = observeDOM(document.body, function(m){
     }
 
     let imageDives = document.querySelectorAll("button[data-testid='rk-wallet-option-metaMask'] div[role='img'] > div");
-    if (imageDives) {
+    if (imageDives && imageDives.length > 0) {
         for (let e of imageDives) {
-            e.style.backgroundImage = "url('https://github.com/Wallet3/Wallet3/blob/main/assets/icon.png?raw=true')"
+            e.style.backgroundImage = "url('https://github.com/Wallet3/Wallet3/blob/ios/assets/icon@128.png?raw=true')"
         }
     }
     
