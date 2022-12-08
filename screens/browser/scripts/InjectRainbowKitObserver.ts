cffa8034287dda3814c2cd0d4d83d805ed129fe4
UnsignedInt8 <unsignedint8@gmail.com>
2022-12-08 14:33:53 +0900

    [UI] Fixed icon and improved performance

--- screens/browser/scripts/InjectRainbowKitObserver.ts
+++ screens/browser/scripts/InjectRainbowKitObserver.ts
@@ -8,15 +8,15 @@ const observerRainbowKit = observeDOM(document.body, function(m){
     let imageDives = document.querySelectorAll("button[data-testid='rk-wallet-option-metaMask'] div[role='img'] > div");
     if (imageDives.length > 0) {
         for (let e of imageDives) {
-            e.style.backgroundImage = "url('https://github.com/Wallet3/Wallet3/blob/ios/assets/icon@128.png?raw=true')"
+            if (e.style.backgroundImage) {
+                e.style.backgroundImage = "url('https://github.com/Wallet3/Wallet3/blob/ios/assets/icon@128.png?raw=true')"
+            }
+        }
+
+        for (let e of document.querySelectorAll("button[data-testid='rk-wallet-option-metaMask'] div *")) {
+            if (e.childElementCount === 0 && e.textContent.toLowerCase() === 'metamask') e.textContent = 'Wallet 3';
         }
     }
-    
-    let rainbowSpan = document.querySelector("button[data-testid='rk-wallet-option-metaMask'] div h2 span");
-    if (rainbowSpan && rainbowSpan.textContent !== 'Wallet 3') {
-        rainbowSpan.textContent = 'Wallet 3';
-    }
-
     
     let connectKitSpans= document.querySelectorAll("#__CONNECTKIT__ button span");
     if (connectKitSpans.length > 0) {
@@ -25,12 +25,14 @@ const observerRainbowKit = observeDOM(document.body, function(m){
                 e.textContent = "Wallet 3";
                 let metamaskButton = e.parentElement;
                 let mmSvg = metamaskButton.querySelector("svg");
+                if (!mmSvg) return;
+                
                 mmSvg.style.display = 'none';
                 let svgContainer = mmSvg.parentElement;
                 let ckImg = document.createElement('img');
                 ckImg.setAttribute("src", "https://github.com/Wallet3/Wallet3/blob/ios/assets/icon@128.png?raw=true");
-                ckImg.setAttribute("width", 32);
-                ckImg.setAttribute("height", 32);
+                ckImg.setAttribute("width", '97.5%');
+                ckImg.setAttribute("height", '97.5%');
                 ckImg.style.borderRadius = '10px';
                 svgContainer.appendChild(ckImg);
                 break;
