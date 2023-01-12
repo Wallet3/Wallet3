a8679dd1e04df2876c69660a925f75376b005d4b
UnsignedInt8 <unsignedint8@gmail.com>
2023-01-12 18:57:20 +0900

    [Bug] Fix KEY3 default address

--- common/apis/Key3did.ts
+++ common/apis/Key3did.ts
@@ -35,7 +35,11 @@ export async function resolveKey3Did(domain: string, chainId: number) {
     const resp: Key3Response<AccountRecords> = await post(`${host}/account/records`, { account: domain });
     if (resp.errno) return '';
 
-    const addr = resp.data?.records.find((r) => r.key === `address.${ChainAddressMap[chainId] || 60}`)?.value || '';
+    const addr =
+      resp.data?.records.find((r) => r.key === `address.${ChainAddressMap[chainId]}`)?.value ||
+      resp.data?.records.find((r) => r.key === 'address.60')?.value ||
+      '';
+
     return isAddress(addr) ? getAddress(addr) : '';
   } catch (error) {}
 
