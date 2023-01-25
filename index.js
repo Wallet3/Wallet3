0e5e3fdb81c12e2cb3c85d5025e2f15a12cbd0f1
UnsignedInt8 <unsignedint8@gmail.com>
2023-01-25 19:46:08 +0900

    [Project] Remove swap on ios, 3.6.0_v2 Release

--- index.js
+++ index.js
@@ -5,14 +5,9 @@ import './configs/polyfill';
 import './configs/debug';
 
 import App from './App';
-import { initWaku } from './viewmodels/tss/Waku';
 import { registerRootComponent } from 'expo';
 
 // registerRootComponent calls AppRegistry.registerComponent('main', () => App);
 // It also ensures that whether you load the app in Expo Go or in a native build,
 // the environment is set up appropriately
 registerRootComponent(App);
-
-if (__DEV__) {
-  initWaku();
-}
