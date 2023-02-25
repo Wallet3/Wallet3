20c4b4a226b836003b9b4f092419744bc492837f
UnsignedInt8 <unsignedint8@gmail.com>
2023-02-25 16:02:01 +0900

    [UI] Create multiSig page

--- screens/land/CreateMultiSigWallet.tsx
+++ screens/land/CreateMultiSigWallet.tsx
@@ -1,23 +1,45 @@
 import { Button, Placeholder, SafeViewContainer } from '../../components';
-import { secureColor, themeColor } from '../../constants/styles';
+import { StyleSheet, Text, View } from 'react-native';
+import { secondaryFontColor, secureColor, themeColor, thirdFontColor } from '../../constants/styles';
 
+import IllustrationNomad from '../../assets/illustrations/tss/nomad.svg';
 import IllustrationPartying from '../../assets/illustrations/misc/partying.svg';
+import IllustrationWorld from '../../assets/illustrations/tss/world.svg';
 import React from 'react';
-import { View } from 'react-native';
+import Swiper from 'react-native-swiper';
 import i18n from '../../i18n';
 import { useNavigation } from '@react-navigation/native';
 
-export default (props) => {
+export default () => {
   const { t } = i18n;
   const navigation = useNavigation<any>();
 
   return (
-    <SafeViewContainer style={{ flex: 1, backgroundColor: '#fff' }}>
-      <Placeholder />
-      <View>
-        <IllustrationPartying width={150} height={150} style={{ alignSelf: 'center' }} />
-      </View>
-      <Placeholder />
+    <SafeViewContainer style={{ flex: 1, backgroundColor: '#fff' }} paddingHeader>
+      <Swiper
+        containerStyle={{ marginHorizontal: -16, flexGrow: 1 }}
+        contentContainerStyle={{ flex: 1, flexGrow: 1 }}
+        autoplay
+        autoplayTimeout={7}
+        paginationStyle={{ marginBottom: -10 }}
+        activeDotColor={secureColor}
+        dotStyle={{ backgroundColor: `${secureColor}40` }}
+      >
+        <View style={styles.viewItem}>
+          <IllustrationPartying width={200} height={200} style={{ alignSelf: 'center', marginBottom: 12 }} />
+          <Text style={styles.txt}>{t('land-create-multi-sig-txt-1')}</Text>
+        </View>
+
+        <View style={styles.viewItem}>
+          <IllustrationWorld width={200} height={200} style={{ alignSelf: 'center', marginBottom: 12 }} />
+          <Text style={styles.txt}>{t('land-create-multi-sig-txt-2')}</Text>
+        </View>
+
+        <View style={styles.viewItem}>
+          <IllustrationNomad width={200} height={200} style={{ alignSelf: 'center', marginBottom: 12 }} />
+          <Text style={styles.txt}>{t('land-create-multi-sig-txt-3')}</Text>
+        </View>
+      </Swiper>
 
       <Button
         title={t('land-welcome-import-wallet')}
@@ -32,3 +54,21 @@ export default (props) => {
     </SafeViewContainer>
   );
 };
+
+const styles = StyleSheet.create({
+  txt: {
+    fontSize: 15,
+    color: thirdFontColor,
+    lineHeight: 19,
+    marginBottom: 12,
+    marginHorizontal: 16,
+    alignSelf: 'center',
+    textAlign: 'center',
+    maxWidth: 320,
+  },
+
+  viewItem: {
+    flex: 1,
+    justifyContent: 'center',
+  },
+});
