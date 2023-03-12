b13ec3d2ce2bab1de61e7375a8784afa78b9ba5e
UnsignedInt8 <unsignedint8@gmail.com>
2023-03-12 15:32:28 +0900

    [UI] Fix margin

--- modals/components/TinyInfo.tsx
+++ modals/components/TinyInfo.tsx
@@ -4,6 +4,7 @@ import { StyleProp, ViewStyle } from 'react-native';
 import { Ionicons } from '@expo/vector-icons';
 import React from 'react';
 import { Text } from 'react-native';
+import { isAndroid } from '../../utils/platform';
 
 interface Props {
   icon: string | 'warning' | 'information-circle';
@@ -16,7 +17,7 @@ interface Props {
 export default ({ icon, color, message, delay, style }: Props) => {
   return (
     <Animated.View style={{ flexDirection: 'row', ...(style as any) }} entering={FadeInDown.delay(delay ?? 0).springify()}>
-      <Ionicons name={icon as any} color={color} style={{ marginEnd: 5 }} />
+      <Ionicons name={icon as any} color={color} style={{ marginEnd: 5, marginTop: isAndroid ? 3 : 1 }} />
       <Text style={{ fontSize: 12, fontWeight: '600', marginEnd: 2, color: color }}>{message}</Text>
     </Animated.View>
   );
