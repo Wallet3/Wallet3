91a034f0d2fed557eb1660d5742463b74427259b
UnsignedInt8 <unsignedint8@gmail.com>
2023-03-16 16:37:02 +0900

    [4337] Refactor activated code

--- viewmodels/account/ERC4337Account.ts
+++ viewmodels/account/ERC4337Account.ts
@@ -1,4 +1,4 @@
-import { makeObservable, observable, runInAction } from 'mobx';
+import { computed, makeObservable, observable, runInAction } from 'mobx';
 
 import { AccountBase } from './AccountBase';
 import AsyncStorage from '@react-native-async-storage/async-storage';
@@ -6,30 +6,32 @@ import { getCode } from '../../common/RPC';
 
 const Keys = {
   accountActivated: (address: string, chainId: number) => `${chainId}_${address}_erc4337_activated`,
+  activated: 'activated',
 };
 
 export class ERC4337Account extends AccountBase {
   readonly type = 'erc4337';
-
-  activated = false;
+  readonly activatedChains = new Map<number, boolean>();
 
   constructor(address: string, index: number, extra?: { signInPlatform?: string }) {
     super(address, index, extra);
-    makeObservable(this, { activated: observable });
+    makeObservable(this, { activatedChains: observable });
   }
 
   async checkActivated(chainId: number) {
-    const info = await AsyncStorage.getItem(Keys.accountActivated(this.address, chainId));
+    if (this.activatedChains.get(chainId)) return true;
 
-    if (info === 'activated') {
-      runInAction(() => (this.activated = true));
-      return;
+    const info = await AsyncStorage.getItem(Keys.accountActivated(this.address, chainId));
+    if (info === Keys.activated) {
+      runInAction(() => this.activatedChains.set(chainId, true));
+      return true;
     }
 
     const code = await getCode(chainId, this.address);
-    if (!code || code === '0x') return;
+    if (!code || code === '0x') return false;
 
-    runInAction(() => (this.activated = true));
-    AsyncStorage.setItem(Keys.accountActivated(this.address, chainId), 'activated');
+    runInAction(() => this.activatedChains.set(chainId, true));
+    AsyncStorage.setItem(Keys.accountActivated(this.address, chainId), Keys.activated);
+    return true;
   }
 }
