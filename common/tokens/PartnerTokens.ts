c292e0eaa2fa46cf3ffbb4497bd347434ed681d1
UnsignedInt8 <unsignedint8@gmail.com>
2023-06-08 21:48:11 +0900

    [UI] Add partner info

--- common/tokens/PartnerTokens.ts
+++ common/tokens/PartnerTokens.ts
@@ -1,7 +1,13 @@
+export const VERSE = {
+  address: '0x249cA82617eC3DfB2589c4c17ab7EC9765350a18',
+  decimals: 18,
+  symbol: 'VERSE',
+};
+
 export const CSS = {
   address: '0x28fB9FCf7cF7a67d05bD19fFBEc17578c0dE63d3',
   decimals: 8,
   symbol: 'CSS',
 };
 
-export const PartnerTokens = [CSS];
+export const PartnerTokens = [VERSE, CSS];
